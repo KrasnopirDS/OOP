3bebb8a9e384850be6642e0ea94190f0ed4b8d94
Daniil Krasnopir <krasnopd@gmail.com>
2024-01-15 20:35:40 +0100

    Lab3

---
+++ OOP_Lab3/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab3
+{
+    internal class Person
+    {
+        private string firstName, lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public virtual string View() => $"{firstName} {lastName} {age} lat";
+    }
+}
