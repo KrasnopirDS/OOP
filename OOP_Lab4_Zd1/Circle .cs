356c5f41e88ee805f9d64519089edea976eaace7
Daniil Krasnopir <krasnopd@gmail.com>
2024-01-15 18:51:49 +0100

    Lab4_zd1

---
+++ OOP_Lab4_Zd1/Circle .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab4_Zd1
+{
+    internal class Circle : Shape
+    {
+        public override void Draw() { Console.WriteLine($"Drawing a circle: x = {X}, y = {Y}, width = {Width}, height = {Height}"); }
+    }
+}
---
+++ OOP_Lab4_Zd1/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab4_Zd1
+{
+    internal class Rectangle : Shape
+    {
+        public override void Draw() { Console.WriteLine($"Drawing a rectangle: x = {X}, y = {Y}, width = {Width}, height = {Height}"); }
+    }
+}
